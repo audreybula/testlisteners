86a8d884730cb540bda86a803767e0ca95fc6ffd
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-11-01 15:35:01 +1200

    email on failure

---
+++ src/test/java/TestConfig.java
@@ -0,0 +1,15 @@
+
+
+public class TestConfig{
+
+	public static String server="smtp.gmail.com";
+	public static String from = "tflrunningsheet@gmail.com";
+	public static String password = "runningsheet123";
+	public static String[] to ={"olly.bula@gmail.com"};
+	public static String subject = "Test Report";
+	
+	public static String messageBody ="TestMessage";
+	public static String attachmentPath=System.getProperty("user.dir")+"/Reports.zip";
+	public static String attachmentName="reports.zip";
+	
+}

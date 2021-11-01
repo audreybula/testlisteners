14ee5d1d6da016864b245310c85295c9329d9aff
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-11-01 15:34:44 +1200

    email zipped screenshots on failure

--- src/test/java/TestListener.java
+++ src/test/java/TestListener.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -15,9 +18,9 @@ public class TestListener extends TestBase implements ITestListener {
 	}
 
 	public void onTestFailure(ITestResult result) {
-		
+
 		String methodName = result.getName().toString().trim();
-		
+
 		try {
 			testUtil.captureScreenshot(methodName);
 		} catch (IOException e) {
@@ -43,7 +46,20 @@ public class TestListener extends TestBase implements ITestListener {
 	}
 
 	public void onFinish(ITestContext context) {
-		// not implemented
+
+		testUtil.zip(System.getProperty("user.dir") + "/screenshot");
+		monitoringMail mail = new monitoringMail();
+		try {
+			mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, TestConfig.messageBody,
+					TestConfig.attachmentPath, TestConfig.attachmentName);
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
